33ada2b9561c18eec68551a06b234d43b594719e
joaquintides <joaquin@tid.es>
2025-03-02 20:52:56 +0100

    added clang-x64 job

--- insert_data.sh
+++ insert_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 python2 insert_data.py gcc-x64/comparison_table.cpp.txt     README.md
+python2 insert_data.py clang-x64/comparison_table.cpp.txt   README.md
 python2 insert_data.py clang-arm64/comparison_table.cpp.txt README.md
